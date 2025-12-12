319635bb61c3d8394ddfc5238d72e4b7080c8b5e
Junsu Jeon <pds.jeon@gmail.com>
2025-12-12 15:15:34 +0900

    Update LibrayMain.cs

--- yahtzee-rules/LibrayMain.cs
+++ yahtzee-rules/LibrayMain.cs
@@ -4,3 +4,27 @@ public class LibrayMain
 {
 
 }
+
+
+
+public enum UpperSection
+{
+    Ones,
+    Twos,
+    Threes,
+    Fours,
+    Fives,
+    Sixes
+};
+
+public enum LowerSection
+{
+    ThreeOfKinds,
+    FourOfKinds,
+    FullHouse,
+    SmallStraight,
+    LargeStraight,
+    Yahtzee,
+    Chance
+}
+
