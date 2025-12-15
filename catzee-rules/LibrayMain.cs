deb477d1bb538f2be0b9e4f83294bf35aad2e27d
Junsu Jeon <dec04th@hotmail.com>
2025-12-15 14:58:46 +0900

    WIP

--- catzee-rules/LibrayMain.cs
+++ catzee-rules/LibrayMain.cs
@@ -1,30 +1,89 @@
-﻿namespace yahtzee_rules;
+﻿using System.ComponentModel;
+
+namespace catzee_rules;
 
 public class LibrayMain
 {
 
 }
 
-
-
-public enum UpperSection
+public class MyDeck
 {
-    Ones,
-    Twos,
-    Threes,
-    Fours,
-    Fives,
-    Sixes
-};
+    public List<int> numbersInDeck = new List<int>();
+    public void AddNumber(int number)
+    {
+        numbersInDeck.Add(number);
+    }
+    public void AddNumberRange(int[] numbers)
+    {
+        numbersInDeck.AddRange(numbers); 
+    }
 
-public enum LowerSection
-{
-    ThreeOfKinds,
-    FourOfKinds,
-    FullHouse,
-    SmallStraight,
-    LargeStraight,
-    Yahtzee,
-    Chance
+    public HandRanking CheckPossibleHandRank()
+    {
+        var groups = numbersInDeck
+           .GroupBy(x => x)
+           .Select(g => g.Count())
+           .OrderByDescending(x => x)
+           .ToArray();
+        // 2. 스트레이트 체크
+        bool isStraight = IsStraight(numbersInDeck.ToArray());
+
+        // 3. 족보 판별
+        //if (groups[0] == 4)
+        //    return HandRanking.FourOfAKind;
+
+        if (groups[0] == 3 && groups[1] == 2)
+            return HandRanking.FullHouse;
+
+        if (isStraight)
+            return HandRanking.Straight;
+
+        if (groups[0] == 3)
+            return HandRanking.ThreeOfAKind;
+
+        if (groups[0] == 2 && groups[1] == 2)
+            return HandRanking.TwoPair;
+
+        if (groups[0] == 2)
+            return HandRanking.OnePair;
+
+        return HandRanking.HighCard;
+
+
+    }
+    private static bool IsStraight(int[] hand)
+    {
+        var sorted = hand
+            .Distinct()
+            .OrderBy(x => x)
+            .ToArray();
+
+        // 중복 있으면 스트레이트 불가
+        if (sorted.Length != 5)
+            return false;
+
+        // 일반 스트레이트
+        for (int i = 1; i < sorted.Length; i++)
+        {
+            if (sorted[i] != sorted[i - 1] + 1)
+                return false;
+        }
+
+        return true;
+    }
 }
 
+
+
+public enum HandRanking
+{
+    HighCard,
+    Straight,
+    OnePair,
+    TwoPair,
+    Flush,
+    FullHouse,
+    StraightFlush,
+    PairFlush
+}
